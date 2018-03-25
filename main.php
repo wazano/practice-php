1adc94d130a055fc5fe2b37b06b2ea1ffd248a15
Hagezawa <hagezawavita@gmail.com>
2018-03-26 03:31:23 +0900

    もう少し書き方を分かりやすく、無名関数は便利

--- main.php
+++ main.php
@@ -2,11 +2,9 @@
     //配列から不要な値を取り除く方法
     $a = [1, 2, 3, 4, 5, 10, 11, 12];
 
-    $result = array_filter($a, function($v) {
-        return $v % 2 == 1;
-    });
-
-    print_r($a);
-
-    print_r($result);
+    $callback = function($v) {
+        $v % 2 == 1;
+    };
+    
+    $result = array_filter($a, $callback);
 ?>
