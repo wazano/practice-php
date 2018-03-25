2f2ed03730b6924ddd0447da0de82c515884fb33
Hagezawa <hagezawavita@gmail.com>
2018-03-26 02:27:26 +0900

    forとforeachで配列の列挙される順番が違う
    驚いた

--- main.php
+++ main.php
@@ -1,14 +1,30 @@
 <?php
 
     $array = array(3, 100, 43, 66, 21, 43);
-    echo $array[1];
+    echo $array[1] . "\n";
 
-    $map = array(
+    $map = [
         "Nami" => 18,
         "Sanji" => 20,
-        "Takashi" => 28
-    );
+        "Nozawa" => 23
+    ];
 
     echo $map["Nami"] . "\n";
-    
+
+    //foreachで配列とmapを列挙すると面白い現象が見れるらしい
+    $a = [0, 1];
+    $a[5] = 5;
+    $a[4] = 4;
+    $a[2] = 2;
+    $a[3] = 3;
+
+    for($i = 0; $i < count($a); $i++) {
+        echo $a[$i] . "\n";
+    }
+
+    //foreach文で配列$aの一覧を表示
+    foreach ($a as $e) {
+        echo $e . "\n";
+    }
+
 ?>
