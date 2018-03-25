995d7b34c011e17d70d5c3554431d9c9c47d6215
Hagezawa <hagezawavita@gmail.com>
2018-03-25 21:32:25 +0900

    配列の初期化
    シンプルで分かりやすい

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+
+    $array = array(3, 100, 43, 66, 21, 43);
+    echo $array[1];
+
+    $map = array(
+        "Nami" => 18,
+        "Sanji" => 20,
+        "Takashi" => 28
+    );
+
+    echo $map["Nami"] . "\n";
+    
+?>
